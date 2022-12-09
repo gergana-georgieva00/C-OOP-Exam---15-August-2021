ec93f2a8220189e637232827071f140585a761e0
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-09 15:00:28 +0200

    C# OOP Exam - 15 August 2021

---
+++ CarRacing/Models/Racers/Racer.cs
@@ -0,0 +1,96 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public class Racer : IRacer
+    {
+        private string username;
+        private string racingBehaviour;
+        private int drivingExperience;
+        private ICar car;
+
+        public Racer(string username, string racingBehavior, int drivingExperience, ICar car)
+        {
+            this.Username = username;
+            this.RacingBehavior = racingBehavior;
+            this.DrivingExperience = drivingExperience;
+            this.Car = car;
+        }
+
+        public string Username 
+        {
+            get => username;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Username cannot be null or empty.");
+                }
+
+                username = value;
+            }
+        }
+
+        public string RacingBehavior
+        {
+            get => racingBehaviour;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Racing behavior cannot be null or empty.");
+                }
+
+                racingBehaviour = value;
+            }
+        }
+
+        public int DrivingExperience
+        {
+            get => drivingExperience;
+            private set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Racer driving experience must be between 0 and 100.");
+                }
+
+                drivingExperience = value;
+            }
+        }
+
+        public ICar Car
+        {
+            get => car;
+            private set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentException("Car cannot be null or empty.");
+                }
+
+                car = value;
+            }
+        }
+
+        public bool IsAvailable()
+            => Car.FuelAvailable >= 0;
+
+        public void Race()
+        {
+            Car.Drive();
+            if (this.GetType().Name == "ProfessionalRacer")
+            {
+                this.DrivingExperience += 10;
+            }
+            else
+            {
+                this.DrivingExperience += 5;
+            }
+        }
+    }
+}
