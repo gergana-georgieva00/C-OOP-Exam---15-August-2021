6c2face34d1f6f0334130f870f6d020aa9900c06
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-09 14:03:28 +0200

    C# OOP Exam - 15 August 2021

---
+++ CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,114 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Cars
+{
+    public abstract class Car : ICar
+    {
+        private string make;
+        private string model;
+        private string vin;
+        private int horsePower;
+        private double fuelAvailable;
+        private double fuelConsumptionPerRace;
+
+        public Car(string make, string model, string vin, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.VIN = vin;
+            this.HorsePower = horsePower;
+            this.FuelAvailable = fuelAvailable;
+            this.FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+
+        public string Make 
+        {
+            get => make;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Car make cannot be null or empty.");
+                }
+
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Car model cannot be null or empty.");
+                }
+
+                model = value;
+            }
+        }
+        public string VIN
+        {
+            get => vin;
+            private set
+            {
+                if (vin.Length != 17)
+                {
+                    throw new ArgumentException("Car VIN must be exactly 17 characters long.");
+                }
+
+                vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => horsePower;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Horse power cannot be below 0.");
+                }
+
+                horsePower = value;
+            }
+        }
+
+        public double FuelAvailable { get; private set; }
+
+        public double FuelConsumptionPerRace
+        {
+            get => fuelConsumptionPerRace;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Fuel consumption cannot be below 0.");
+                }
+            }
+        }
+
+        public void Drive()
+        {
+            if (FuelAvailable - FuelConsumptionPerRace < 0)
+            {
+                FuelAvailable = 0;
+            }
+            else
+            {
+                FuelAvailable -= FuelConsumptionPerRace;
+            }
+
+            if (this.GetType().Name == "TunedCar")
+            {
+                var result = HorsePower - (HorsePower * 0.03);
+                HorsePower = (int)Math.Round(result);
+            }
+        }
+    }
+}
