494e917ad824592fbe04111dbbe3826e1592095f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-09 15:34:05 +0200

    C# OOP Exam - 15 August 2021

---
+++ CarRacing/Repositories/CarRepository.cs
@@ -0,0 +1,37 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarRacing.Repositories
+{
+    public class CarRepository : IRepository<ICar>
+    {
+        private List<ICar> models;
+
+        public CarRepository()
+        {
+            models = new List<ICar>();
+        }
+
+        public IReadOnlyCollection<ICar> Models => models.AsReadOnly();
+
+        public void Add(ICar model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentException("Cannot add null in Car Repository");
+            }
+
+            models.Add(model);
+        }
+
+        public ICar FindBy(string property)
+            => models.FirstOrDefault(c => c.VIN == property);
+
+        public bool Remove(ICar model)
+            => models.Remove(model);
+    }
+}
